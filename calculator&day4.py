2276360341b11103e73ce44f1b1488031a98516a
Saraj Dawar <sarajdawar13@gmail.com>
2026-03-15 00:08:33 +0530

    Added new practice files

---
+++ calculator&day4.py
@@ -0,0 +1,10 @@
+print("Mini Calculator")
+
+num1=float(input("Enter your number: "))
+num2=float(input("Enter your number: "))
+
+
+print("Addition:",num1+num2)
+print("Subtraction:",num1-num2 )
+print("Multiplication:",num1*num2)
+print("Divide:", num1/num2)
---
+++ day-5.py
@@ -0,0 +1,27 @@
+a ="10"
+b = "5"
+print (a == b)
+print (a != b)
+print( a < b)
+print (a > b)
+print (a <= b)
+print (a >= b)
+
+
+
+ #CHALLENGE
+age=int(input(" Enter your age: "))
+
+print(age>18)
+
+
+number=int(input(" Enter your no: "))
+
+print(number>100)
+
+
+
+
+
+
+
